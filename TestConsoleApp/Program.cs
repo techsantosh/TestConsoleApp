71b7b8e83386be73fa7b3e44db2168d8416cfc00
Santosh Phape <santoshp@hoc.com>
2018-12-11 18:58:06 +0530

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = @"Provider=PostgreSQL OLE DB Provider;Data Source=192.10.44.97;location=RPM;User ID=postgres;password=Pass@123;";//"Provider=PostgreSQL OLE DB Provider;Server=192.10.44.97;Database=RPM;User=postgres;Password=Pass@123;";
+
+            string queryString = "select * from Public.\"HOCUser\"";
+         
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    OleDbCommand command = new OleDbCommand(queryString, connection);
+                    connection.Open();
+                    OleDbDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // Insert code to process data.
+                    }
+                    reader.Close();
+                }
+          
+            Console.WriteLine("Done!!!");
+            Console.ReadKey();
+        }
+    }
+}
